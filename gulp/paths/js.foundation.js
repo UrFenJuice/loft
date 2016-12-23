08c9b64ab241e7fcca29d237aa9196493ff0dc02
UrFenJuice <wuin69@gmail.com>
2016-12-23 10:06:38 +0300

    add jquery validation

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -5,5 +5,6 @@ module.exports = [
   './bower_components/owl.carousel/dist/owl.carousel.min.js',
   './node_modules/jquery.formstyler/jquery.formstyler.min.js',
   './node_modules/jquery.scrollbar/jquery.scrollbar.min.js',
+  './node_modules/jquery-validation/dist/jquery.validate.min.js',
   './source/js/masonry.pkgd.min.js'
 ];
